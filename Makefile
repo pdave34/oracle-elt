12eb2652cb5dff7f59df5dc149dd9302af781958
Pranav Dave <75449103+pdave34@users.noreply.github.com>
2025-10-09 20:04:13 -0400

    added calendar

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: setup-oracle stop-oracle start-oracle dbt-debug
+.PHONY: setup-oracle stop-oracle start-oracle dbt-debug dbt-build dbt-test
 
 setup-oracle:
 	docker run -d --name oracle-19c \
@@ -16,4 +16,16 @@ stop-oracle:
 	docker stop oracle-19c
 
 dbt-debug:
+	export ORA_PYTHON_DRIVER_TYPE=thick && \
+	export ORACLE_HOME=/Users/astropd/Projects/oracle/instantclient_23_3 && \
 	dbt debug --profiles-dir ./elt --project-dir ./elt
+
+dbt-build:
+	export ORA_PYTHON_DRIVER_TYPE=thick && \
+	export ORACLE_HOME=/Users/astropd/Projects/oracle/instantclient_23_3 && \
+	dbt build --profiles-dir ./elt --project-dir ./elt
+
+dbt-test:
+	export ORA_PYTHON_DRIVER_TYPE=thick && \
+	export ORACLE_HOME=/Users/astropd/Projects/oracle/instantclient_23_3 && \
+	dbt test --profiles-dir ./elt --project-dir ./elt
---
+++ elt/models/example/calendar.sql
@@ -0,0 +1,28 @@
+
+{{
+  config(
+    materialized='table',
+    full_refresh=True,
+    post_hook='CREATE INDEX {{ "idx_" ~ this.name ~ "_date" }} ON {{ this }} ("date")'
+  )
+}}
+
+with date_series as (
+  select
+    (to_date('{{ var("start_date", "2000-01-01") }}', 'YYYY-MM-DD') + level - 1) as calendar_date
+  from
+    dual
+  connect by level <= to_date('{{ var("end_date", "2099-12-31") }}', 'YYYY-MM-DD') - to_date('{{ var("start_date", "2000-01-01") }}', 'YYYY-MM-DD') + 1
+)
+select
+  calendar_date as "date",
+  to_number(to_char(calendar_date, 'YYYY')) as "year",
+  to_number(to_char(calendar_date, 'MM')) as "month",
+  to_number(to_char(calendar_date, 'DD')) as "day",
+  to_number(to_char(calendar_date, 'Q')) as "quarter",
+  to_number(to_char(calendar_date, 'WW')) as "week",
+  to_number(to_char(calendar_date, 'ID')) as "isodow",
+  to_number(to_char(calendar_date, 'DDD')) as "doy",
+  to_number(to_char(calendar_date, 'IYYY')) as "isoyear"
+from
+  date_series
